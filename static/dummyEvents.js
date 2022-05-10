367a572cb8ea5d0ba0cfd913e6d479e0e63788e8
SahithShetty <sahith.tunuguntla1@gmail.com>
2022-05-10 23:16:50 +0530

    First commit

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+    {
+      price: 0.1,
+      from: 'Dhiraj',
+      to: 'Bhargav',
+      date: '12 hrs ago',
+    },
+    {
+      price: 0.1,
+      from: 'Bhargav',
+      to: 'Dhiraj',
+      date: '13 hrs ago',
+    },
+    {
+      price: 0.1,
+      from: 'Dhiraj',
+      to: 'Bhargav',
+      date: '15 hrs ago',
+    },
+    {
+      price: 0.1,
+      from: 'Bhargav',
+      to: 'Dhiraj',
+      date: '20 hrs ago',
+    },
+  ]
